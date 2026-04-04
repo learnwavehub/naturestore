18cae80ffd5cdb0778cd28f8f5d9f58069a4b241
learnwavehub <webdesigns091@gmail.com>
2026-04-04 14:18:49 +0300

    Initial commit

--- lib/mongoDB.ts
+++ lib/mongoDB.ts
@@ -12,7 +12,7 @@ export const connectToDB = async (): Promise<void> => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL || "", {
-      dbName: "microplug_Store"
+      dbName: "mionatech_Store"
     })
 
     isConnected = true;
